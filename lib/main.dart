7531528d88902cee898199cc379fb9006bc16e7f
eastvin1 <103909484+eastvin1@users.noreply.github.com>
2023-07-01 11:54:14 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:tugas_1/home_page.dart';
+import 'package:tugas_1/named_route.dart';
+import 'package:tugas_1/route_generator.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final Map<String, WidgetBuilder> routes = {
+    '/': (context) => HomePage(),
+    '/named': (context) => NamedRoute(),
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      onGenerateRoute: RouteGenerator.generateRoute,
+    );
+  }
+}
+
+// Buat Class untuk RouteGenerator
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    return MaterialPageRoute(builder: (_) => GeneratorRoute());
+  }
+}
